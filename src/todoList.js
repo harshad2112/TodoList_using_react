aff2927cb7bf145e0a56ac1ba9fb86b7fdc33011
harshad <harshadaggarwal1999@gmail.com>
2021-01-18 22:03:45 +0530

    added some css

--- src/todoList.js
+++ src/todoList.js
@@ -2,12 +2,15 @@ import React from "react"
 
 function TodoList(props){
     return(
-    <div class="todo-div">
+    <div className="todo-div">
         <input
             type="checkbox" checked={props.data.completed}
             onChange={() => props.handleClick(props.data.id) }
         />
+        <div class="text">
         {props.data.text}
+
+        </div>
     </div>)
 }
 export default TodoList
